2657dc24d474e092984b924829e39a794f8f5e2b
iwag <iwagame@gmail.com>
2016-12-24 14:18:49 +0900

    import react-autosuggest

--- pages/about/index.js
+++ pages/about/index.js
@@ -8,26 +8,133 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React from 'react';
+
+ import styles from './styles.css';
+
+import React, { Component, PropTypes } from 'react';
+
+import Autosuggest from 'react-autosuggest';
+import AutosuggestHighlightMatch from 'autosuggest-highlight/match';
+import AutosuggestHighlightParse from 'autosuggest-highlight/parse';
+
 import Layout from '../../components/Layout';
-import s from './styles.css';
-import { title, html } from './index.md';
 
-class AboutPage extends React.Component {
 
-  componentDidMount() {
-    document.title = title;
+const people = [
+  {
+    first: 'Charlie',
+    last: 'Brown',
+    twitter: 'dancounsell'
+  },
+  {
+    first: 'Charlotte',
+    last: 'White',
+    twitter: 'mtnmissy'
+  },
+  {
+    first: 'Chloe',
+    last: 'Jones',
+    twitter: 'ladylexy'
+  },
+  {
+    first: 'Cooper',
+    last: 'King',
+    twitter: 'steveodom'
   }
+];
 
-  render() {
-    return (
-      <Layout className={s.content}>
-        <h1>{title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-      </Layout>
-    );
-  }
-
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+function escapeRegexCharacters(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
 
-export default AboutPage;
+function getSuggestions(value) {
+  const escapedValue = escapeRegexCharacters(value.trim());
+
+  if (escapedValue === '') {
+    return [];
+  }
+
+  const regex = new RegExp('\\b' + escapedValue, 'i');
+
+  return people.filter(person => regex.test(getSuggestionValue(person)));
+}
+
+function getSuggestionValue(suggestion) {
+  return `${suggestion.first} ${suggestion.last}`;
+}
+
+function renderSuggestion(suggestion, { query }) {
+  const suggestionText = `${suggestion.first} ${suggestion.last}`;
+  const matches = AutosuggestHighlightMatch(suggestionText, query);
+  const parts = AutosuggestHighlightParse(suggestionText, matches);
+
+  return (
+    <span className={styles.suggestionContent/* + suggestion.twitter*/}>
+      <span className={styles.name}>
+        {
+          parts.map((part, index) => {
+            const className = part.highlight ? styles.highlight : null;
+
+            return (
+              <span className={className} key={index}>{part.text}</span>
+            );
+          })
+        }
+      </span>
+    </span>
+  );
+}
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: '',
+      suggestions: []
+    };
+  }
+
+  onChange = (event, { newValue, method }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: "Type 'c'",
+      value,
+      onChange: this.onChange
+    };
+
+    return (
+      <Layout>
+      <Autosuggest
+        theme={styles}
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps} />
+        </Layout>
+    );
+  }
+}
+
+//ReactDOM.render(<App />, document.getElementById('app'));
