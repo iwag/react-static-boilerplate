53ddc2e129fb4dfed75dd3905a06c8fb2f135078
iwag <takuya_iwakami@dwango.co.jp>
2016-12-28 18:11:57 +0900

    iroiro

--- pages/about/index.js
+++ pages/about/index.js
@@ -24,6 +24,9 @@ import video_recent from './video_dump.js';
 import video_top from './video_mylist_dump.js';
 import live_top from './live_dump.js';
 import news_top from './news_dump.js';
+import video_tag_recent from './video_tag_recent.js';
+import video_tag_top from './video_tag_top.js';
+
 
 // https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
 function escapeRegexCharacters(str) {
@@ -39,66 +42,90 @@ function getSuggestions(value) {
 
   var targets = [
     {
-      title: "tags"
+      title: "タグ",
+      type: "tag"
     },
     {
-      "title": "channels"
+      title: "チャンネル",
+      type: "tag"
     },
     {
-      "title": "lives"
+      "title": "生放送",
+      type: "live"
     },
     {
-      "title": "videos(人気)"
+      "title": "人気ビデオ",
+      type: "video"
     },
     {
-      "title": "videos(新着)"
+      "title": "新着ビデオ",
+      type: "video"
     },
     {
-      "title": "news"
+      "title": "ニュース",
+      type: "news"
     }
   ];
-  escapedValue = escapedValue.toLowerCase();
-  if (escapedValue.length == 1 ) {
+  const escapedValueLong = escapedValue.toLowerCase();
+  const isTag = escapedValueLong.length > 2;
+  if (escapedValueLong.length == 1 ) {
     ;
   } else {
-    escapedValue = (escapedValue[0] + escapedValue[1]);
+    escapedValue = (escapedValueLong[0] + escapedValueLong[1]);
   }
+
   targets[0]["suggests"] = tag_top[escapedValue].map(i=>{
-      return {"type": "tag",    "name": i.tag, "thumbnailUrl": ""  };
+      return {"type": "tag",    "name": i.tag, "count" :i.tag_counter, "thumbnailUrl": ""  };
   });
 
+
   // channel
-  targets[1]["title"] += " (" + channel_top[escapedValue].meta.totalCount + ")";
+  targets[1]["count"] = channel_top[escapedValue].meta.totalCount;
   targets[1]["suggests"] = channel_top[escapedValue].data.map(i=>{
-      return {"type": "channel",    "name": i.title, "id":i.contentId,  "thumbnailUrl": i.communityIcon || i.thumbnailUrl  };
+      return {"type": "channel",    "name": i.title, "id":i.contentId,  "thumbnailUrl": i.thumbnailUrl  };
   });
 
   // live
-  targets[2]["title"] += " (" + live_top[escapedValue].meta.totalCount + ")";
+  targets[2]["count"] =live_top[escapedValue].meta.totalCount;
   targets[2]["suggests"] = live_top[escapedValue].data.map(i=>{
       return {"type": "live",    "name": i.title, "id":i.contentId, "thumbnailUrl": i.communityIcon || i.thumbnailUrl  };
   });
 
   // video top
-  targets[3]["suggests"] = video_top[escapedValue].data.map(i=>{
-      return {"type": "video",    "name": i.title, "id":i.contentId, "thumbnailUrl": i.thumbnailUrl };
-  });
+  const videoEscapedValue = (video_tag_top[escapedValueLong]) ? escapedValueLong : tag_top[escapedValue][0].tag.toUpperCase();
+  if (isTag && video_tag_top[videoEscapedValue]) {
+    targets[3]["suggests"] = video_tag_top[videoEscapedValue].data.map(i=>{
+        return {"type": "video",    "name": i.title, "id":i.contentId, "thumbnailUrl": i.thumbnailUrl };
+    });
+  } else {
+    targets[3]["suggests"] = video_top[escapedValue].data.map(i=>{
+        return {"type": "video",    "name": i.title, "id":i.contentId, "thumbnailUrl": i.thumbnailUrl };
+    });
+  }
+
   // video recent
-  targets[4]["title"] += " (" + video_recent[escapedValue].meta.totalCount + ")";
-  targets[4]["suggests"] = video_recent[escapedValue].data.map(i=>{
-      return {"type": "video",    "name": i.title, "id":i.contentId, "thumbnailUrl": i.thumbnailUrl };
-  });
+  if (isTag && video_tag_recent[videoEscapedValue]) {
+    targets[4]["count"] = video_tag_recent[videoEscapedValue].meta.totalCount;
+    targets[4]["suggests"] = video_tag_recent[videoEscapedValue].data.map(i=>{
+        return {"type": "video",    "name": i.title, "id":i.contentId, "thumbnailUrl": i.thumbnailUrl };
+    });
+  } else {
+    targets[4]["count"] = video_recent[escapedValue].meta.totalCount;
+    targets[4]["suggests"] = video_recent[escapedValue].data.map(i=>{
+        return {"type": "video",    "name": i.title, "id":i.contentId, "thumbnailUrl": i.thumbnailUrl };
+    });
+  }
 
   // news
-  targets[5]["title"] += " (" + news_top[escapedValue].meta.totalCount + ")";
+  targets[5]["count"] = news_top[escapedValue].meta.totalCount;
   targets[5]["suggests"] = news_top[escapedValue].data.map(i=>{
-      return {"type": "news",    "name": i.title, "id":i.contentId, "thumbnailUrl": "http://p.news.nimg.jp/photo/" + i.thumbnailKey };
+      return {"type": "news",    "name": i.title, "id":i.contentId, "thumbnailUrl": "http://p.news.nimg.jp/photo/" + i.thumbnailKey + "p.jpg" };
   });
 
 
   const regex = new RegExp('\\b' + escapedValue, 'i');
 
-  targets[0]["suggests"] = targets[0]["suggests"].filter(person => regex.test(getSuggestionValue(person))).slice(0,3);
+  targets[0]["suggests"] = targets[0]["suggests"].filter(person => regex.test(getSuggestionValue(person))).slice(0,5);
 
   return targets.filter(section => section.suggests.length > 0);
 }
@@ -108,17 +135,21 @@ const getSuggestionValue = suggestion => suggestion.name;
 const getSectionSuggestions = section => section.suggests;
 
 const renderSectionTitle = section => (
-  <strong>{section.title}</strong>
+  <span>
+    <span className={styles.sectionType}>{section.title}</span>
+    <span className={styles.pill}>{section.count}</span>
+    <span className={styles.section_more}><a className={styles.title} href={"http://search.nicovideo.jp/"+section.type+"/search/"}>続きをみる</a></span>
+</span>
 );
 
 function renderSuggestion(suggestion, { query }) {
-  const suggestionText = `${suggestion.name}`;
+  const suggestionText = suggestion.type=="tag" ? `${suggestion.name} (${suggestion.count}+)` : suggestion.name;
   const matches = AutosuggestHighlightMatch(suggestionText, query);
   const parts = AutosuggestHighlightParse(suggestionText, matches);
 
   return (
     <span className={styles.suggestionContent}>
-      <a href={suggestion.type=="tag" ? "./" :  "https://nico.ms/" + suggestion.id}>
+      <a className={styles.title} href={suggestion.type=="tag" ? "http://search.nicovideo.jp/video/tag/" + suggestion.name :  "https://nico.ms/" + suggestion.id}>
       <img className={styles.thumbnail} src={suggestion.thumbnailUrl}/>
       <span className={styles.name}>
         {
@@ -189,7 +220,8 @@ export default class App extends React.Component {
         renderSuggestion={renderSuggestion}
         renderSectionTitle={renderSectionTitle}
         getSectionSuggestions={getSectionSuggestions}
-        focusInputOnSuggestionClick={true}
+        alwaysRenderSuggestions={true}
+focusInputOnSuggestionClick={false}        
         inputProps={inputProps} />
         </Layout>
     );
