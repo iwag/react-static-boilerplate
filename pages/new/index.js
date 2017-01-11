efa2ee699e29b5dea59a4616705c25980cc14f62
iwag <iwagame@gmail.com>
2017-01-12 00:17:03 +0900

    snack bar in /new

--- pages/new/index.js
+++ pages/new/index.js
@@ -2,32 +2,30 @@
 import React, { PropTypes } from 'react';
 import Layout from '../../components/Layout';
 import $ from 'jquery';
-import { Switch, Chip, Checkbox, IconButton, Grid, Icon, Cell  } from 'react-mdl';
+import { Switch, Chip, Checkbox, IconButton, Grid, Icon, Cell, Snackbar  } from 'react-mdl';
 import config from '../../components/Config';
 
 class NewPage extends React.Component {
 
-  componentDidMount() {
-    document.title = "title";
-  }
-
-  render() {
-    return (
-      <Layout style={{}}>
-      単語を入力
-      <Register url={config.host + "/v1/word.json"} />
-      </Layout>
-    );
-  }
-
-}
-
-export default NewPage;
-
-class Register extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleShowSnackbar = this.handleShowSnackbar.bind(this);
+    this.handleTimeoutSnackbar = this.handleTimeoutSnackbar.bind(this);
+    this.state = { isSnackbarActive: false };
+    this.url=config.host + "/v1/word.json";
+  }
+
+  handleShowSnackbar() {
+    this.setState({ isSnackbarActive: true });
+  }
+  handleTimeoutSnackbar() {
+    this.setState({ isSnackbarActive: false });
+  }
+
+  componentDidMount() {
+    document.title = "title";
   }
 
   handleSubmit(e) {
@@ -37,26 +35,36 @@ class Register extends React.Component {
     };
     $.ajax({
       type: 'post',
-      url: this.props.url,
+      url: this.url,
       contentType: 'application/json',
       data: JSON.stringify(word),
       success: function(data) {
         this.refs.text.value = "";
+        this.handleShowSnackbar();
       }.bind(this),
       error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
+        console.error(this.url, status, err.toString());
       }.bind(this)
     });
   }
 
   render() {
     return (
+      <Layout style={{}}>
+      単語を入力
       <form onSubmit={this.handleSubmit.bind(this)}>
         <div className="mdl-textfield mdl-js-textfield" style={{display:"table-cell", padding: "5px 0"}}>
-          <textarea className="mdl-textfield__input" type="text" rows= "3" ref="text" name="text" style={{"font-size": 3+"em", width: 725+"pt", border:"1px solid rgba(0,0,0,.12)"}} ></textarea>
+          <textarea className="mdl-textfield__input" type="text" rows= "3" ref="text" name="text" style={{width: 525+"pt","font-size": 3+"em", border:"1px solid rgba(0,0,0,.12)"}} ></textarea>
         </div>
         <button type="submit" className="mdl-button mdl-js-button" style={{width: 100+"pt"}}>Submit</button>
       </form>
+      <Snackbar active={this.state.isSnackbarActive} onTimeout={this.handleTimeoutSnackbar} timeout={1500}>
+        Done...
+      </Snackbar>
+      </Layout>
     );
   }
+
 }
+
+export default NewPage;
