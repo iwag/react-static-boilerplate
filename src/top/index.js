654fd893ea9a7d5da6b58f15ab96be367cdd22a0
iwag <iwagame@gmail.com>
2017-02-18 21:35:23 +0900

    modify login method

--- src/top/index.js
+++ src/top/index.js
@@ -24,7 +24,7 @@ class AboutPage extends React.Component {
     return (
       <Layout className={s.content}>
         <div dangerouslySetInnerHTML={{ __html: html }} />
-        <a className="mdl-navigation__link" href={ config.host + "/v1/login"}>Googleアカウントでログイン</a><br />
+        <a className="mdl-navigation__link" href={ config.host + "/v1/login"}>ログイン</a><br />
         <div className="mdl-layout-spacer"></div>
       </Layout>
     );
